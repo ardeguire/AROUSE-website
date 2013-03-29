59e44aca5c68875ce2a91f400e11eeacd91bc066
root <root@ardeguire.com>
2013-03-29 09:06:13 -0500

    Added a page of useful links and info for DJs.

---
+++ djstuff/index.php
@@ -0,0 +1,41 @@
+<?php include '../inc/html-head'; ?>
+<style>ul>li {line-height:150%;font-size:16px;}</style>
+</head>
+	<body>
+		<div class="container-fluid">
+		<div class="row-fluid" style="margin-top:10px;margin-bottom:30px;">
+			<div class='hero-unit'>
+			<h1>DJ Stuff</h1>
+			<h3>Useful informaiton to make your show awesome</h3>
+			</div>
+		</div>
+		<div class="row-fluid">
+		<div class="outer-round" id="matte" style="padding:10px;">
+		<div class="inner-round" id="readable" style="margin:0px;">
+		<ul>
+			<li><a href="AROUSERules.pdf">The Rules of AROUSE</a> (pdf)</li>
+			<li><a href="AROUSEConstitution2013-03-25.pdf">The Constitution of AROUSE</a> (pdf)</li>
+			<li><a href="GarageBandInstructions.pdf">Whisper Room recording instructions</a> (pdf)</li>
+			<li><a href="LiveShowInstructions.html">live show recording instructions</a></li>
+			<li><a href="troubleshooting.html">troubleshooting steps</a></li>
+			<li><a href="https://docs.google.com/spreadsheet/viewform?formkey=dGdHYWVUa3h4dnRQcEhYWXZLdDBpX0E6MQ#gid=0">DJ Form</a> (you should fill this out whenever you do a show or play a pre-record)</li>
+			<li>contact list for:
+			<ul>
+				<li>executive board</li>
+				<li><a href="https://docs.google.com/spreadsheet/ccc?key=0Am2dQmTAMVM1dG9DRkd4SVJQRDdqRW9BUzNtWWxwZmc&usp=sharing#gid=0">DJs</a></li>
+			</ul>
+			</li>
+			<li>Jason's powerpoints (pdf)
+				<ul>
+					<li><a href="PROGRAMMING_101_PRESENTATION_3-1-13.pdf">Programming 101</a></li>
+					<li><a href="TALENT_PRESENTATION_3-1-13.pdf">Talent 101</a></li>
+					<li><a href="Demotapes.pdf">Demotapes and Getting A Job</a></li>
+				</ul></li>
+			<li>link to the playlist maker (eventually)</li>
+		</ul>
+		</div><!-- end inner-round -->
+		</div><!-- end outer-round -->
+		</div><!-- end row -->
+		</div><!-- end container -->
+	</body>
+</html>
