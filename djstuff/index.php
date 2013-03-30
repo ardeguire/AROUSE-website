d69ac9c43318c9928abf2a6aa729dd7eadcbca0d
root <root@ardeguire.com>
2013-03-30 12:50:38 -0500

    Update ordering to put the more important, on-air resources at the top.

--- djstuff/index.php
+++ djstuff/index.php
@@ -12,12 +12,10 @@
 		<div class="row-fluid">
 		<div class="outer-round" id="matte" style="padding:10px;">
 		<div class="inner-round" id="readable" style="margin:0px;">
+		<h3>On-Air Resources</h3>
 		<ul>
-			<li><a href="AROUSERules.pdf">The Rules of AROUSE</a> (pdf)</li>
-			<li><a href="AROUSEConstitution2013-03-25.pdf">The Constitution of AROUSE</a> (pdf)</li>
-			<li><a href="GarageBandInstructions.pdf">Whisper Room recording instructions</a> (pdf)</li>
-			<li><a href="LiveShowInstructions.html">live show recording instructions</a></li>
 			<li><a href="troubleshooting.html">troubleshooting steps</a></li>
+			<li><a href="LiveShowInstructions.html">live show recording instructions</a></li>
 			<li><a href="https://docs.google.com/spreadsheet/viewform?formkey=dGdHYWVUa3h4dnRQcEhYWXZLdDBpX0E6MQ#gid=0">DJ Form</a> (you should fill this out whenever you do a show or play a pre-record)</li>
 			<li>contact list for:
 			<ul>
@@ -25,13 +23,20 @@
 				<li><a href="https://docs.google.com/spreadsheet/ccc?key=0Am2dQmTAMVM1dG9DRkd4SVJQRDdqRW9BUzNtWWxwZmc&usp=sharing#gid=0">DJs</a></li>
 			</ul>
 			</li>
+			<li>link to the playlist maker (eventually)</li>
+		</ul>
+
+		<h3>Reference Documents</h3>
+		<ul>
+			<li><a href="AROUSERules.pdf">The Rules of AROUSE</a> (pdf)</li>
+			<li><a href="AROUSEConstitution2013-03-25.pdf">The Constitution of AROUSE</a> (pdf)</li>
+			<li><a href="GarageBandInstructions.pdf">Whisper Room recording instructions</a> (pdf)</li>
 			<li>Jason's powerpoints (pdf)
 				<ul>
 					<li><a href="PROGRAMMING_101_PRESENTATION_3-1-13.pdf">Programming 101</a></li>
 					<li><a href="TALENT_PRESENTATION_3-1-13.pdf">Talent 101</a></li>
 					<li><a href="Demotapes.pdf">Demotapes and Getting A Job</a></li>
 				</ul></li>
-			<li>link to the playlist maker (eventually)</li>
 		</ul>
 		</div><!-- end inner-round -->
 		</div><!-- end outer-round -->
